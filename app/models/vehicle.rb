ec4eb9aea1b7633b9318d1a3d90cef71fd57fca8
Luke Niedermyer <luke@westarete.com>
2018-07-05 18:03:06 -0400

    Move US state methods into Vehicle model
    It could end up in a concern at some point, but the 
    helper isn’t the right place and a full-blown State
    model seems like overkill at this point.

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,11 +1,72 @@
 class Vehicle < ApplicationRecord
 
+  def self.us_states_options
+    [
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arizona', 'AZ'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['District of Columbia', 'DC'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawaii', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Mississippi', 'MS'],
+      ['Missouri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['New Hampshire', 'NH'],
+      ['New Jersey', 'NJ'],
+      ['New Mexico', 'NM'],
+      ['New York', 'NY'],
+      ['North Carolina', 'NC'],
+      ['North Dakota', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregon', 'OR'],
+      ['Pennsylvania', 'PA'],
+      ['Puerto Rico', 'PR'],
+      ['Rhode Island', 'RI'],
+      ['South Carolina', 'SC'],
+      ['South Dakota', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY']
+    ]
+  end
+
+  def self.us_state_abbreviations
+    Vehicle.us_states_options.map{|_name, abb| abb}
+  end
+
   validates :license_state,
             :license_number,
             presence: true
 
   validates :license_state,
-            inclusion: { in: us_state_abbreviations }
+            inclusion: { in: Vehicle.us_state_abbreviations }
 
   validates :license_number,
             uniqueness: { scope: :license_state,
@@ -14,5 +75,4 @@ class Vehicle < ApplicationRecord
 
   validates :license_number,
             format: { with: /\A[a-zA-Z0-9]{4,7}\z/, message: "is invalid format" }
-
 end
--- spec/component/models/vehicle_spec.rb
+++ spec/component/models/vehicle_spec.rb
@@ -1,5 +1,4 @@
 require 'component/component_spec_helper'
-include StatesHelper
 
 describe Vehicle, type: :model do
 
@@ -17,7 +16,7 @@ describe Vehicle, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:license_state) }
     it { is_expected.to validate_presence_of(:license_number) }
-    it { is_expected.to validate_inclusion_of(:license_state).in_array(us_state_abbreviations) }
+    it { is_expected.to validate_inclusion_of(:license_state).in_array(Vehicle.us_state_abbreviations) }
 
 
     it 'should validate that :license_number is case-insensitively unique within the scope of :license_state' do
@@ -43,4 +42,80 @@ describe Vehicle, type: :model do
     end
   end
 
+  describe '.us_states_options' do
+    it 'returns an array of arrays containing US states and their abbreviations' do
+      expect(Vehicle.us_states_options).to eq [
+                                        ['Alabama', 'AL'],
+                                        ['Alaska', 'AK'],
+                                        ['Arizona', 'AZ'],
+                                        ['Arkansas', 'AR'],
+                                        ['California', 'CA'],
+                                        ['Colorado', 'CO'],
+                                        ['Connecticut', 'CT'],
+                                        ['Delaware', 'DE'],
+                                        ['District of Columbia', 'DC'],
+                                        ['Florida', 'FL'],
+                                        ['Georgia', 'GA'],
+                                        ['Hawaii', 'HI'],
+                                        ['Idaho', 'ID'],
+                                        ['Illinois', 'IL'],
+                                        ['Indiana', 'IN'],
+                                        ['Iowa', 'IA'],
+                                        ['Kansas', 'KS'],
+                                        ['Kentucky', 'KY'],
+                                        ['Louisiana', 'LA'],
+                                        ['Maine', 'ME'],
+                                        ['Maryland', 'MD'],
+                                        ['Massachusetts', 'MA'],
+                                        ['Michigan', 'MI'],
+                                        ['Minnesota', 'MN'],
+                                        ['Mississippi', 'MS'],
+                                        ['Missouri', 'MO'],
+                                        ['Montana', 'MT'],
+                                        ['Nebraska', 'NE'],
+                                        ['Nevada', 'NV'],
+                                        ['New Hampshire', 'NH'],
+                                        ['New Jersey', 'NJ'],
+                                        ['New Mexico', 'NM'],
+                                        ['New York', 'NY'],
+                                        ['North Carolina', 'NC'],
+                                        ['North Dakota', 'ND'],
+                                        ['Ohio', 'OH'],
+                                        ['Oklahoma', 'OK'],
+                                        ['Oregon', 'OR'],
+                                        ['Pennsylvania', 'PA'],
+                                        ['Puerto Rico', 'PR'],
+                                        ['Rhode Island', 'RI'],
+                                        ['South Carolina', 'SC'],
+                                        ['South Dakota', 'SD'],
+                                        ['Tennessee', 'TN'],
+                                        ['Texas', 'TX'],
+                                        ['Utah', 'UT'],
+                                        ['Vermont', 'VT'],
+                                        ['Virginia', 'VA'],
+                                        ['Washington', 'WA'],
+                                        ['West Virginia', 'WV'],
+                                        ['Wisconsin', 'WI'],
+                                        ['Wyoming', 'WY']
+                                      ]
+    end
+  end
+
+  describe '.us_state_abbreviations' do
+    it 'returns an array state abbreviations' do
+      expect(Vehicle.us_state_abbreviations).to eq %w(
+                                               AL AK AZ AR CA
+                                               CO CT DE DC FL
+                                               GA HI ID IL IN
+                                               IA KS KY LA ME
+                                               MD MA MI MN MS
+                                               MO MT NE NV NH
+                                               NJ NM NY NC ND
+                                               OH OK OR PA PR
+                                               RI SC SD TN TX
+                                               UT VT VA WA WV
+                                               WI WY
+                                              )
+    end
+  end
 end
